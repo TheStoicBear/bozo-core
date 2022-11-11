ea71af03f045563dea7739984eed22923f63a81e
helmimarif <helmimarif11@gmail.com>
2022-11-11 10:29:08 +0700

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,5 +1,26 @@
 AyseCore = {}
+AyseCore.SelectedCharacter = nil
+AyseCore.Characters = {}
+AyseCore.Functions = {}
+AyseCore.Config = config
 
 function GetCoreObject()
     return AyseCore
 end
+
+function AyseCore.Functions.GetSelectedCharacter(cb)
+    if not cb then return AyseCore.SelectedCharacter end
+    cb(AyseCore.SelectedCharacter)
+end
+
+function AyseCore.Functions.GetCharacters(cb)
+    if not cb then return AyseCore.Characters end
+    cb(AyseCore.Characters)
+end
+
+AddEventHandler("playerSpawned", function()
+    if config.enablePVP then
+        SetCanAttackFriendly(PlayerPedId(), true, false)
+        NetworkSetFriendlyFireOption(true)
+    end
+end)
