ce09444afa9c702e54896a568a1132e3f3d24d3b
helmimarif <helmimarif11@gmail.com>
2022-11-11 10:31:54 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,7 @@
 config = {
     serverName = "AyseFramework"
+    characterLimit = 1, -- How many characters the player can create.
+    enablePVP = true, -- pvp allows doing damage to other players.
+    startingCash = 2500, -- default cash the character will start with if the character creator doesn't specify it.
+    startingBank = 8000, -- default money in the bank account the character will start with if the character creator doesn't specify it.
 }
