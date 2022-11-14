cde5324c2e32e73be8434e14d49d02eb9b10f9ee
helmimarif <helmimarif11@gmail.com>
2022-11-14 09:18:07 +0700

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -18,9 +18,28 @@ function AyseCore.Functions.GetCharacters(cb)
     cb(AyseCore.Characters)
 end
 
+if config.customPauseMenu then
+    Citizen.CreateThread(function()
+        while true do
+            Citizen.Wait(0)
+            if AyseCore.SelectedCharacter then
+                if IsPauseMenuActive() then
+                    BeginScaleformMovieMethodOnFrontendHeader("SET_HEADING_DETAILS")
+                    AddTextEntry("FE_THDR_GTAO", config.serverName) 
+                    ScaleformMovieMethodAddParamPlayerNameString(AyseCore.SelectedCharacter.firstName .. " " .. AyseCore.SelectedCharacter.lastName)
+                    PushScaleformMovieFunctionParameterString("Cash: $" .. tostring(AyseCore.SelectedCharacter.cash))
+                    PushScaleformMovieFunctionParameterString("Bank: $" .. tostring(AyseCore.SelectedCharacter.bank))
+                    EndScaleformMovieMethod()
+                end
+            end
+        end
+    end)
+end
+
 AddEventHandler("playerSpawned", function()
     if config.enablePVP then
         SetCanAttackFriendly(PlayerPedId(), true, false)
         NetworkSetFriendlyFireOption(true)
     end
+    print("^0AyseFramework")
 end)
