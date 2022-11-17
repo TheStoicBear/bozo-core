4c7b73627c268a90cbe5013b8ef63b99ce317d0d
helmimarif <helmimarif11@gmail.com>
2022-11-17 11:07:07 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,21 @@
 author "helmimarif"
 description "AyseFramework Core"
-version "3.0"
+version "4.0"
 
 fx_version "cerulean"
 game "gta5"
 lua54 "yes"
 
 shared_script "config.lua"
-client_scripts "client/main.lua"
+client_scripts {
+    "client/main.lua",
+    "client/events.lua"
+}
 
 server_scripts {
     "@oxmysql/lib/MySQL.lua",
     "server/main.lua",
+    "server/commands.lua",
     "server/functions.lua",
     "server/events.lua"
 }
