7c0074fdf5784b71849626fbc00a8951fe9584ae
helmimarif <helmimarif11@gmail.com>
2022-12-26 09:24:59 +0700

    Update events.lua

--- server/events.lua
+++ server/events.lua
@@ -4,7 +4,7 @@ AddEventHandler("playerConnecting", function(name, setKickReason, deferrals)
 
     deferrals.defer()
     Wait(0)
-    deferrals.update("Connecting to discord.")
+    deferrals.update("Connecting to discord...")
     Wait(0)
 
     if not discordIdentifier then
@@ -30,7 +30,7 @@ AddEventHandler("onResourceStart", function(resourceName)
     if (GetCurrentResourceName() ~= resourceName) then
         return
     end
-    MySQL.query("CREATE TABLE IF NOT EXISTS characters ( `character_id` INT(10) NOT NULL AUTO_INCREMENT, `license` VARCHAR(200) NOT NULL DEFAULT '0', `first_name` VARCHAR(50) NULL DEFAULT NULL, `last_name` VARCHAR(50) NULL DEFAULT NULL, `dob` VARCHAR(50) NULL DEFAULT NULL, `gender` VARCHAR(50) NULL DEFAULT NULL, `cash` INT(10) NULL DEFAULT '0', `bank` INT(10) NULL DEFAULT '0', `phone_number` VARCHAR(20) NULL DEFAULT NULL, `groups` LONGTEXT NULL DEFAULT '[]', `last_location` LONGTEXT NULL DEFAULT '[]',  `clothing` LONGTEXT NULL DEFAULT '[]', PRIMARY KEY (`character_id`) USING BTREE);")
+    MySQL.query("CREATE TABLE IF NOT EXISTS characters ( `character_id` INT(10) NOT NULL AUTO_INCREMENT, `license` VARCHAR(200) NOT NULL DEFAULT '0', `first_name` VARCHAR(50) NULL DEFAULT NULL, `last_name` VARCHAR(50) NULL DEFAULT NULL, `dob` VARCHAR(50) NULL DEFAULT NULL, `gender` VARCHAR(50) NULL DEFAULT NULL,`twt` VARCHAR(50) NULL DEFAULT NULL, `job` VARCHAR(50) NULL DEFAULT NULL, `cash` INT(10) NULL DEFAULT '0', `bank` INT(10) NULL DEFAULT '0', `phone_number` VARCHAR(20) NULL DEFAULT NULL, `groups` LONGTEXT NULL DEFAULT '[]', `last_location` LONGTEXT NULL DEFAULT '[]',  `clothing` LONGTEXT NULL DEFAULT '[]', PRIMARY KEY (`character_id`) USING BTREE);")
     print('^4Ayse_Core ^0Database structure validated!')
 end)
 
@@ -41,14 +41,14 @@ end)
 
 RegisterNetEvent("Ayse:newCharacter", function(newCharacter)
     local player = source
-    AyseCore.Functions.CreateCharacter(player, newCharacter.firstName, newCharacter.lastName, newCharacter.dob, newCharacter.gender, newCharacter.cash, newCharacter.bank)
+    AyseCore.Functions.CreateCharacter(player, newCharacter.firstName, newCharacter.lastName, newCharacter.dob, newCharacter.gender, newCharacter.twt, newCharacter.job, newCharacter.cash, newCharacter.bank)
 end)
 
 RegisterNetEvent("Ayse:editCharacter", function(newCharacter)
     local player = source
     local characters = AyseCore.Functions.GetPlayerCharacters(player)
     if not characters[newCharacter.id] then return end
-    AyseCore.Functions.UpdateCharacterData(newCharacter.id, newCharacter.firstName, newCharacter.lastName, newCharacter.dob, newCharacter.gender)
+    AyseCore.Functions.UpdateCharacterData(newCharacter.id, newCharacter.firstName, newCharacter.lastName, newCharacter.dob, newCharacter.gender, newCharacter.twt, newCharacter.job)
 end)
 
 RegisterNetEvent("Ayse:deleteCharacter", function(characterId)
@@ -76,7 +76,7 @@ AddEventHandler("playerDropped", function()
     if character then
         AyseCore.Functions.UpdateLastLocation(character.id, character.lastLocation)
     end
-    TriggerEvent("Ayse:characterUnloaded", player)
+    TriggerEvent("Ayse:characterUnloaded", player, character)
     character = nil
 end)
 
