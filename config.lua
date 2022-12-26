ad55f3262c57da26ff239abd6adf52412f464a72
helmimarif <helmimarif11@gmail.com>
2022-12-26 09:25:43 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -11,10 +11,20 @@ config = {
     notWhitelistedMessage = "You're not allowlisted in this server please join our discord to apply for a allowlist: https://discord.gg/TZfCJXKtYx",
     whitelistRoles = {
         "1029724677632954420"
-    }
+    },
+
+    enableRichPresence = true,
+    updateIntervall = 60,
+    appId = 1029306790120280114,
+    largeLogo = "1024",
+    smallLogo = "512",
+    firstButtonName = "DISCORD",
+    firstButtonLink = "https://discord.gg/TZfCJXKtYx",
+    secondButtonName = "JOIN",
+    secondButtonLink = "https://cfx.re/join/yybb9k"
 }
 
 server_config = {
     discordServerToken = "MTAyOTMwNjc5MDEyMDI4MDExNA.GWnUbN.tLaHbyj7DFsacYFb8yVdjvb992osDy_bIZ4Y2c",
-    guildId = "1029723778009280523",
+    guildId = "1029723778009280523"
 }
