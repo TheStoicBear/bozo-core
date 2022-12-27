8d97bd251265a112d5425f3ac3941ce578df9362
helmimarif <helmimarif11@gmail.com>
2022-12-27 18:22:45 +0700

    Create functions.lua

---
+++ client/functions.lua
@@ -0,0 +1,11 @@
+function GetCoreObject()
+    return AyseCore
+end
+
+function AyseCore.Functions.GetSelectedCharacter()
+    return AyseCore.SelectedCharacter
+end
+
+function AyseCore.Functions.GetCharacters()
+    return AyseCore.Characters
+end
