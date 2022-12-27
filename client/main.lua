cf1ec5f7372c35ba7cd930bd1f81d4f4905da2ca
helmimarif <helmimarif11@gmail.com>
2022-12-27 18:23:03 +0700

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -4,20 +4,6 @@ AyseCore.Characters = {}
 AyseCore.Functions = {}
 AyseCore.Config = config
 
-function GetCoreObject()
-    return AyseCore
-end
-
-function AyseCore.Functions.GetSelectedCharacter(cb)
-    if not cb then return AyseCore.SelectedCharacter end
-    cb(AyseCore.SelectedCharacter)
-end
-
-function AyseCore.Functions.GetCharacters(cb)
-    if not cb then return AyseCore.Characters end
-    cb(AyseCore.Characters)
-end
-
 if config.enableRichPresence then
     Citizen.CreateThread(function()
         while true do
