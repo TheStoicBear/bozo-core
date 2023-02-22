7ad9ac1ae3e8b499e9351ce86ccf55867769af75
helmimarif <helmimarif11@gmail.com>
2023-02-22 10:38:27 +0700

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -9,20 +9,6 @@ function GetCoreObject()
     return AyseCore
 end
 
-CreateThread(function()
-    while true do
-        Wait(30000)
-        for player, playerInfo in pairs(AyseCore.Players) do
-            local ped = GetPlayerPed(player)
-            if DoesEntityExist(ped) then
-                local lastLocation = GetEntityCoords(ped)
-                playerInfo.lastLocation = {x = lastLocation.x, y = lastLocation.y, z = lastLocation.z}
-                TriggerClientEvent("Ayse:updateLastLocation", player, playerInfo.lastLocation)
-            end
-        end
-    end
-end)
-
 isResourceStarted("ox_inventory", function(started)
     if not started then return end
     SetConvarReplicated("inventory:framework", "ayse")
