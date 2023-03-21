2cbd7eccbd067f314cf88c7d65a007876598b0b9
helmimarif <helmimarif11@gmail.com>
2023-03-21 18:26:20 +0700

    Update events.lua

--- server/events.lua
+++ server/events.lua
@@ -1,6 +1,6 @@
 AddEventHandler("playerConnecting", function(name, setKickReason, deferrals)
     local player = source
-    local discordIdentifier = AyseCore.Functions.GetPlayerIdentifierFromType("discord", player)
+    local discordIdentifier = AFCore.Functions.GetPlayerIdentifierFromType("discord", player)
 
     deferrals.defer()
     Wait(0)
@@ -10,81 +10,69 @@ AddEventHandler("playerConnecting", function(name, setKickReason, deferrals)
     if not discordIdentifier then
         deferrals.done("Your discord isn't connected to FiveM, make sure discord is open and restart FiveM.")
     else
-        if config.enableDiscordWhitelist then
-            local discordUserId = discordIdentifier:gsub("discord:", "")
-            local discordInfo = AyseCore.Functions.GetUserDiscordInfo(discordUserId)
-            for _, whitelistRole in pairs(config.whitelistRoles) do
-                if whitelistRole == 0 or whitelistRole == "0" or (discordInfo and discordInfo.roles[whitelistRole]) then
-                    deferrals.done()
-                    break
-                end
-            end
-            deferrals.done(config.notWhitelistedMessage)
-        else
-            deferrals.done()
-        end
+        deferrals.done()
     end
 end)
 
-RegisterNetEvent("Ayse:GetCharacters", function()
+RegisterNetEvent("af:GetCharacters", function()
     local player = source
-    TriggerClientEvent("Ayse:returnCharacters", player, AyseCore.Functions.GetPlayerCharacters(player))
+    TriggerClientEvent("af:returnCharacters", player, AFCore.Functions.GetPlayerCharacters(player))
 end)
 
-RegisterNetEvent("Ayse:newCharacter", function(newCharacter)
+RegisterNetEvent("af:newCharacter", function(newCharacter)
     local player = source
-    AyseCore.Functions.CreateCharacter(player, newCharacter.firstName, newCharacter.lastName, newCharacter.dob, newCharacter.gender, newCharacter.cash, newCharacter.bank)
+    AFCore.Functions.CreateCharacter(player, newCharacter.firstName, newCharacter.lastName, newCharacter.dob, newCharacter.gender, newCharacter.cash, newCharacter.bank)
 end)
 
-RegisterNetEvent("Ayse:editCharacter", function(newCharacter)
+RegisterNetEvent("af:editCharacter", function(newCharacter)
     local player = source
-    local characters = AyseCore.Functions.GetPlayerCharacters(player)
+    local characters = AFCore.Functions.GetPlayerCharacters(player)
     if not characters[newCharacter.id] then return end
-    AyseCore.Functions.UpdateCharacterData(newCharacter.id, newCharacter.firstName, newCharacter.lastName, newCharacter.dob, newCharacter.gender)
+    AFCore.Functions.UpdateCharacterData(newCharacter.id, newCharacter.firstName, newCharacter.lastName, newCharacter.dob, newCharacter.gender)
 end)
 
-RegisterNetEvent("Ayse:deleteCharacter", function(characterId)
+RegisterNetEvent("af:deleteCharacter", function(characterId)
     local player = source
-    local characters = AyseCore.Functions.GetPlayerCharacters(player)
+    local characters = AFCore.Functions.GetPlayerCharacters(player)
     if not characters[characterId] then return end
-    AyseCore.Functions.DeleteCharacter(characterId)
+    AFCore.Functions.DeleteCharacter(characterId)
 end)
 
-RegisterNetEvent("Ayse:setCharacterOnline", function(id)
+RegisterNetEvent("af:setCharacterOnline", function(id)
     local player = source
-    local characters = AyseCore.Functions.GetPlayerCharacters(player)
+    local characters = AFCore.Functions.GetPlayerCharacters(player)
     if not characters[id] then return end
-    AyseCore.Functions.SetActiveCharacter(player, id)
+    AFCore.Functions.SetActiveCharacter(player, id)
 end)
 
-RegisterNetEvent("Ayse:updateClothes", function(clothing)
+RegisterNetEvent("af:updateClothes", function(clothing)
     local player = source
-    local character = AyseCore.Players[player]
-    AyseCore.Functions.SetPlayerData(character.id, "clothing", clothing)
+    local character = AFCore.Players[player]
+    AFCore.Functions.SetPlayerData(character.id, "clothing", clothing)
 end)
 
-RegisterNetEvent("Ayse:exitGame", function()
+RegisterNetEvent("af:exitGame", function()
     local player = source
     DropPlayer(player, "Disconnected.")
 end)
 
 AddEventHandler("playerDropped", function()
     local player = source
-    local character = AyseCore.Players[player]
+    local character = AFCore.Players[player]
     if character then
         local ped = GetPlayerPed(player)
         local lastLocation = GetEntityCoords(ped)
-        AyseCore.Functions.UpdateLastLocation(character.id, {x = lastLocation.x, y = lastLocation.y, z = lastLocation.z})
+        AFCore.Functions.UpdateLastLocation(character.id, {x = lastLocation.x, y = lastLocation.y, z = lastLocation.z})
     end
-    TriggerEvent("Ayse:characterUnloaded", player, character)
+    TriggerEvent("af:characterUnloaded", player, character)
     character = nil
 end)
 
 AddEventHandler("playerJoining", function()
     local src = source
-    local discordUserId = AyseCore.Functions.GetPlayerIdentifierFromType("discord", src):gsub("discord:", "")
-    local discordInfo = AyseCore.Functions.GetUserDiscordInfo(discordUserId)
-    AyseCore.PlayersDiscordInfo[src] = discordInfo
+    local discordUserId = AFCore.Functions.GetPlayerIdentifierFromType("discord", src):gsub("discord:", "")
+    local discordInfo = AFCore.Functions.GetUserDiscordInfo(discordUserId)
+    AFCore.PlayersDiscordInfo[src] = discordInfo
 end)
 
 AddEventHandler("onResourceStart", function(resourceName)
@@ -92,11 +80,11 @@ AddEventHandler("onResourceStart", function(resourceName)
         return
     end 
     Wait(1000)
-    if not next(AyseCore.PlayersDiscordInfo) then
+    if not next(AFCore.PlayersDiscordInfo) then
         for _, playerId in ipairs(GetPlayers()) do
-            local discordUserId = AyseCore.Functions.GetPlayerIdentifierFromType("discord", playerId):gsub("discord:", "")
-            local discordInfo = AyseCore.Functions.GetUserDiscordInfo(discordUserId)
-            AyseCore.PlayersDiscordInfo[tonumber(playerId)] = discordInfo
+            local discordUserId = AFCore.Functions.GetPlayerIdentifierFromType("discord", playerId):gsub("discord:", "")
+            local discordInfo = AFCore.Functions.GetUserDiscordInfo(discordUserId)
+            AFCore.PlayersDiscordInfo[tonumber(playerId)] = discordInfo
         end
     end
 end)
