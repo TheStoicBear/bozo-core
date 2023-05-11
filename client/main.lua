d2b2a0d4f01edb46aae0fbe214014e61e71e1456
helmimarif <helmimarif11@gmail.com>
2023-05-11 20:52:22 +0700

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -4,42 +4,37 @@ AFCore.Characters = {}
 AFCore.Functions = {}
 AFCore.Config = config
 
-if config.enableRichPresence then
-    Citizen.CreateThread(function()
-        while true do
-            if AFCore.SelectedCharacter then
-                SetDiscordAppId(config.appId)
-                SetRichPresence("FiveM character framework")
-                SetDiscordRichPresenceAsset(config.largeLogo)
-                SetDiscordRichPresenceAssetText(config.serverName)
-                SetDiscordRichPresenceAssetSmall(config.smallLogo)
-                SetDiscordRichPresenceAssetSmallText("Playing as: " .. AFCore.SelectedCharacter.firstName .. " " .. AFCore.SelectedCharacter.lastName)
-                SetDiscordRichPresenceAction(0, config.firstButtonName, config.firstButtonLink)
-                SetDiscordRichPresenceAction(1, config.secondButtonName, config.secondButtonLink)
-            end
-            Citizen.Wait(60000)
+-- Discord Rich Presence
+Citizen.CreateThread(function()
+    while true do
+        if AFCore.SelectedCharacter then
+            SetDiscordAppId(1029306790120280114)   -- Discord API App Id
+            SetRichPresence(AFCore.SelectedCharacter.job .. " - " .. AFCore.SelectedCharacter.firstName .. " " .. AFCore.SelectedCharacter.lastName)
+            SetDiscordRichPresenceAsset("icon")   -- Rich Presence Asset Name
+            SetDiscordRichPresenceAssetText(config.serverName)
+            SetDiscordRichPresenceAction(0, "DISCORD", "https://discord.gg/qG2Xsm8gAz")     -- Rich Presence First Button Display
+            SetDiscordRichPresenceAction(1, "JOIN", "https://cfx.re/join/yybb9k")   -- Rich Presence Second Button Display
         end
-    end)
-end
+        Citizen.Wait(60000)     -- Status Update Delay
+    end
+end)
 
-if config.customPauseMenu then
-    Citizen.CreateThread(function()
-        while true do
-            Citizen.Wait(0)
-            if AFCore.SelectedCharacter then
-                if IsPauseMenuActive() then
-                    BeginScaleformMovieMethodOnFrontendHeader("SET_HEADING_DETAILS")
-                    AddTextEntry("FE_THDR_GTAO", config.serverName) 
-                    ScaleformMovieMethodAddParamPlayerNameString(AFCore.SelectedCharacter.firstName .. " " .. AFCore.SelectedCharacter.lastName)
-                    PushScaleformMovieFunctionParameterString("Cash: $" .. tostring(AFCore.SelectedCharacter.cash))
-                    PushScaleformMovieFunctionParameterString("Bank: $" .. tostring(AFCore.SelectedCharacter.bank))
-                    EndScaleformMovieMethod()
-                end
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if AFCore.SelectedCharacter then
+            if IsPauseMenuActive() then
+                BeginScaleformMovieMethodOnFrontendHeader("SET_HEADING_DETAILS")
+                AddTextEntry("FE_THDR_GTAO", config.serverName) 
+                ScaleformMovieMethodAddParamPlayerNameString(AFCore.SelectedCharacter.firstName .. " " .. AFCore.SelectedCharacter.lastName)
+                PushScaleformMovieFunctionParameterString("Cash: $" .. tostring(AFCore.SelectedCharacter.cash))
+                PushScaleformMovieFunctionParameterString("Bank: $" .. tostring(AFCore.SelectedCharacter.bank))
+                EndScaleformMovieMethod()
             end
         end
-    end)
-end
+    end
+end)
 
 AddEventHandler("playerSpawned", function()
-    print("^0AyseFramework")
+    print("AyseFramework")
 end)
