a51f1f8deffbe66d09841135fb96a52384891ddc
The Stoic Bear <112611821+TheStoicBear@users.noreply.github.com>
2023-07-10 16:39:13 -0700

    Update events.lua

--- client/events.lua
+++ client/events.lua
@@ -1,20 +1,20 @@
-RegisterNetEvent("af:returnCharacters", function(characters)
+RegisterNetEvent("bozo:returnCharacters", function(characters)
     BozoCore.Characters = characters
 end)
 
-RegisterNetEvent("af:updateMoney", function(cash, bank)
+RegisterNetEvent("bozo:updateMoney", function(cash, bank)
     BozoCore.SelectedCharacter.cash = cash
     BozoCore.SelectedCharacter.bank = bank
 end)
 
-RegisterNetEvent("af:setCharacter", function(character)
+RegisterNetEvent("bozo:setCharacter", function(character)
     BozoCore.SelectedCharacter = character
 end)
 
-RegisterNetEvent("af:updateCharacter", function(character)
+RegisterNetEvent("bozo:updateCharacter", function(character)
     BozoCore.SelectedCharacter = character
 end)
 
-RegisterNetEvent("af:updateLastLocation", function(location)
+RegisterNetEvent("bozo:updateLastLocation", function(location)
     BozoCore.SelectedCharacter.lastLocation = location
 end)
