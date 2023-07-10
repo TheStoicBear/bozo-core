596190e4b1f788b7d4f3f35c83736b8e1bbf0f8a
The Stoic Bear <112611821+TheStoicBear@users.noreply.github.com>
2023-07-10 16:38:52 -0700

    Update commands.lua

--- server/commands.lua
+++ server/commands.lua
@@ -1,7 +1,7 @@
 BozoCore.Functions.AddCommand("setmoney", "Admin command, manage player money.", function(source, args, rawCommand)
     if not BozoCore.Functions.IsPlayerAdmin(source) then
         return
-        TriggerClientEvent("af-core:notify", source, "You dont have access to this command", "error")
+        TriggerClientEvent("bozo-core:notify", source, "You dont have access to this command", "error")
     end
     local target = tonumber(args[1])
     local action = args[2]
@@ -30,22 +30,22 @@ end, true, {
 BozoCore.Functions.AddCommand("setjob", "Admin command, set player job.", function(source, args, rawCommand)
     if not BozoCore.Functions.IsPlayerAdmin(source) then
         return
-        TriggerClientEvent("af-core:notify", source, "You dont have access to this command", "error")
+        TriggerClientEvent("bozo-core:notify", source, "You dont have access to this command", "error")
     end
     local target = tonumber(args[1])
     if not target or GetPlayerPing(target) == 0 then
         return
-        TriggerClientEvent("af-core:notify", source, "Target player not found", "error")
+        TriggerClientEvent("bozo-core:notify", source, "Target player not found", "error")
     end
     local job = args[2]
     if not job then
         return
-        TriggerClientEvent("af-core:notify", source, "Job required", "error")
+        TriggerClientEvent("bozo-core:notify", source, "Job required", "error")
     end
     local character = BozoCore.Functions.GetPlayer(target)
     BozoCore.Functions.SetPlayerJob(character.id, job, args[3])
     return
-    TriggerClientEvent("af-core:notify", source, GetPlayerName(target) .. " job set to " .. job .. (args[3] and " rank " .. args[3] or " rank 1"), "success")
+    TriggerClientEvent("bozo-core:notify", source, GetPlayerName(target) .. " job set to " .. job .. (args[3] and " rank " .. args[3] or " rank 1"), "success")
 end, false, {
     { name="player", help="Player server id" },
     { name="job name" },
@@ -55,28 +55,28 @@ end, false, {
 BozoCore.Functions.AddCommand("setgroup", "Admin command, set player group.", function(source, args, rawCommand)
     if not BozoCore.Functions.IsPlayerAdmin(source) then
         return
-        TriggerClientEvent("af-core:notify", source, "You dont have access to this command", "error")
+        TriggerClientEvent("bozo-core:notify", source, "You dont have access to this command", "error")
     end
     local target = tonumber(args[1])
     if not target or GetPlayerPing(target) == 0 then
         return
-        TriggerClientEvent("af-core:notify", source, "Target player not found", "error")
+        TriggerClientEvent("bozo-core:notify", source, "Target player not found", "error")
     end
     local group = args[3]
     if not group then
         return
-        TriggerClientEvent("af-core:notify", source, "Group required", "error")
+        TriggerClientEvent("bozo-core:notify", source, "Group required", "error")
     end
     local character = BozoCore.Functions.GetPlayer(target)
     if args[2] == "remove" then
         BozoCore.Functions.RemovePlayerFromGroup(character.id, group)
         return
-        TriggerClientEvent("af-core:notify", source, GetPlayerName(target) .. " removed from " .. group, "success")
+        TriggerClientEvent("bozo-core:notify", source, GetPlayerName(target) .. " removed from " .. group, "success")
     elseif args[2] == "add" then
         local rank = args[4]
         BozoCore.Functions.SetPlayerToGroup(character.id, group, rank)
         return
-        TriggerClientEvent("af-core:notify", source, GetPlayerName(target) .. " added to " .. group .. (rank and " rank " .. rank or " rank 1"), "success")
+        TriggerClientEvent("bozo-core:notify", source, GetPlayerName(target) .. " added to " .. group .. (rank and " rank " .. rank or " rank 1"), "success")
     end
 end, false, {
     { name="player", help="Player server id" },
